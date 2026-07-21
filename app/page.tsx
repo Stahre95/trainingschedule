2eda7cf5a6ca792543b235c9a1c318a634df9576
Johan <johan@bk30.se>
2026-07-21 10:04:43 +0200

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import { ScheduleView } from './components/schedule-view';
+import { readScheduleData } from './lib/schedule-storage';
 
-export default function Home() {
-  return <ScheduleView />;
+export const dynamic = 'force-dynamic';
+
+export default async function Home() {
+  const schedule = await readScheduleData();
+  const initialError = schedule ? '' : 'Schemat gick inte att ladda från servern.';
+
+  return <ScheduleView initialSchedule={schedule} initialError={initialError} />;
 }
